1f008cc879af296e187539b1b637c546325fadc1
eswai <sakamoto.yuki@gmail.com>
2020-04-03 22:26:47 +0900

    Gullwing

---
+++ keyboards/eswai/gullwing/gullwing.h
@@ -0,0 +1,16 @@
+#pragma once
+#include "quantum.h"
+
+#define LAYOUT( \
+  L01, L02, L03, L04, L05, L06,            R01, R02, R03, R04, R05, R06, \
+  L07, L08, L09, L10, L11, L12, L13,  R07, R08, R09, R10, R11, R12, R13, \
+                 L14, L15, L16, L17,  R14, R15, R16, R17, \
+  L18,                L19, L20, L21,  R18, R19, R20,                R21 \
+  ) \
+  { \
+    { L01,   L02,   L03,   L04, L05, L06, KC_NO, KC_NO, R01, R02, R03, R04,   R05,   R06   }, \
+    { L07,   L08,   L09,   L10, L11, L12, L13,   R07,   R08, R09, R10, R11,   R12,   R13   }, \
+    { KC_NO, KC_NO, KC_NO, L14, L15, L16, L17,   R14,   L15, L16, R17, KC_NO, KC_NO, KC_NO }, \
+    { KC_NO, KC_NO, KC_NO, L18, L19, L20, L21,   R18,   R19, R20, R21, KC_NO, KC_NO, KC_NO }, \
+  }
+
---
+++ keyboards/eswai/gullwing/master/config.h
@@ -0,0 +1,14 @@
+#pragma once
+
+#include "../board/custom_board.h"
+
+#define THIS_DEVICE_ROWS 4
+#define THIS_DEVICE_COLS 14
+
+#define ENABLE_STARTUP_ADV_NOLIST
+
+#define MATRIX_ROW_PINS { 19, 17, 15, 13 }
+#define MATRIX_COL_PINS { 25, 26, 27, 28, 29, 30, 31, 10, 11, 12, 14, 16, 18, 20 }
+
+#define IS_LEFT_HAND  true
+//#define USE_AS_I2C_SLAVE
