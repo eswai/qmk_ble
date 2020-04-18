93ea30e74de783190aee0cf8b180a8944fb90430
eswai <sakamoto.yuki@gmail.com>
2020-04-18 21:29:33 +0900

    default keymap

--- keyboards/eswai/gullwing/keymaps/default/keymap.c
+++ keyboards/eswai/gullwing/keymaps/default/keymap.c
@@ -18,6 +18,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #include QMK_KEYBOARD_H
 #include "app_ble_func.h"
 #include <stdio.h>
+#include "keymap_jp.h"
 
 enum custom_keycodes {
     AD_WO_L = SAFE_RANGE, /* Start advertising without whitelist  */
@@ -47,7 +48,7 @@ enum custom_keycodes {
 extern keymap_config_t keymap_config;
 
 enum {
-  _SRLBY,
+  _QWERTY,
   _LOWER,
   _RAISE,
   _ADJUST,
@@ -61,22 +62,76 @@ enum {
 #define XXXXXXX KC_NO
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-/* _SRLBY
+/* _QWERTY
   +-----+-----+-----+-----+-----+-----+-----+-----+-----+-----+-----+-----+-----+-----+
-  |  '  |  ,  |  S  |  R  |  L  |  B  |     |     |  Y  |BSPC |  I  |  D  |  .  |  X  |
+  |  X  |  Q  |  W  |  E  |  R  |  T  |     |     |  Y  |  U  |  I  |  O  |  P  |  .  |
   +-----+-----+-----+-----+-----+-----+-----+-----+-----+-----+-----+-----+-----+-----+
-  |  V  |  W  |  H  |  T  |  E  |  M  |  :  |  ¥  |  P  |  N  |  A  |  O  |  K  |  Q  |
+  |  Z  |  A  |  S  |  D  |  F  |  G  |  :  |  _  |  H  |  J  |  K  |  L  |  ;  |  /  |
   +-----+-----+-----+-----+-----+-----+-----+-----+-----+-----+-----+-----+-----+-----+
-  |     |     |     |  G  |  C  |  U  |  (  |  )  |  U  |  F  |  J  |     |     |     |
+  |     |     |     |  C  |  V  |  B  |  [  |  ]  |  N  |  M  |  ,  |     |     |     |
   +-----+-----+-----+-----+-----+-----+-----+-----+-----+-----+-----+-----+-----+-----+
   |LCMD |     |     |     |LOWER| SPC | TAB |LALT | ENT |RAISE|     |     |     |LCTL |
   +-----+-----+-----+-----+-----+-----+-----+-----+-----+-----+-----+-----+-----+-----+
 */
-  [_SRLBY] = LAYOUT(
-    KC_QUOT,KC_COMM,KC_S   ,KC_R   ,KC_L   ,KC_B   ,                KC_Y   ,KC_BSPC,KC_I   ,KC_D   ,KC_DOT ,KC_X   , \
-    KC_V   ,KC_W   ,KC_H   ,KC_T   ,KC_E   ,KC_M   ,KC_COLN,KC_JYEN,KC_P   ,KC_N   ,KC_A   ,KC_O   ,KC_K   ,KC_Q   , \
-                            KC_G   ,KC_C   ,KC_U   ,KC_LPRN,KC_RPRN,KC_U   ,KC_F   ,KC_J        , \
+  [_QWERTY] = LAYOUT(
+    KC_X   ,KC_Q   ,KC_W   ,KC_E   ,KC_R   ,KC_T   ,                KC_Y   ,KC_U   ,KC_I   ,KC_O   ,KC_P   ,JP_DOT , \
+    KC_Z   ,KC_A   ,KC_S   ,KC_D   ,KC_F   ,KC_G   ,JP_COLN,JP_UNDS,KC_H   ,KC_J   ,KC_K   ,KC_L   ,JP_SCLN,JP_SLSH, \
+                            KC_C   ,KC_V   ,KC_B   ,JP_LBRC,JP_RBRC,KC_N   ,KC_M   ,JP_COMM     , \
     KC_LCMD,                        LOWER  ,KC_SPC ,KC_TAB ,KC_LALT,KC_ENT ,RAISE  ,                        KC_LCTL
+  ),
+
+/* _LOWER
+  +-----+-----+-----+-----+-----+-----+-----+-----+-----+-----+-----+-----+-----+-----+
+  | ESC |  {  |  [  |  (  |  <  |  :  |     |     |  7  |  8  |  9  |  +  |  *  |  =  |
+  +-----+-----+-----+-----+-----+-----+-----+-----+-----+-----+-----+-----+-----+-----+
+  |     |  }  |  ]  |  )  |  >  |  ;  |     |  .  |  4  |  5  |  6  |  -  |  /  |  ,  |
+  +-----+-----+-----+-----+-----+-----+-----+-----+-----+-----+-----+-----+-----+-----+
+  |     |     |     |     |     |     |     |  0  |  1  |  2  |  3  |     |     |     |
+  +-----+-----+-----+-----+-----+-----+-----+-----+-----+-----+-----+-----+-----+-----+
+  | __  |     |     |     | __  | __  | __  | __  | __  | __  |     |     |     | __  |
+  +-----+-----+-----+-----+-----+-----+-----+-----+-----+-----+-----+-----+-----+-----+
+*/
+  [_LOWER] = LAYOUT(
+    KC_ESC ,JP_LCBR,JP_LBRC,JP_LPRN,JP_LT  ,JP_COLN,                KC_7   ,KC_8   ,KC_9   ,JP_PLUS,JP_ASTR,JP_EQL , \
+    XXXXXXX,JP_RCBR,JP_RBRC,JP_RPRN,JP_GT  ,JP_SCLN,XXXXXXX,JP_DOT ,KC_4   ,KC_5   ,KC_6   ,JP_MINS,JP_SLSH,JP_COMM, \
+                            XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,KC_0   ,KC_1   ,KC_2   ,KC_3        , \
+    _______,                        _______,_______,_______,_______,_______,_______,                        _______
+  ),
+
+/* _RAISE
+  +-----+-----+-----+-----+-----+-----+-----+-----+-----+-----+-----+-----+-----+-----+
+  |  |  |  ~  |  @  |  #  |  $  |  %  |     |     |     | UP  |     |HOME |     |     |
+  +-----+-----+-----+-----+-----+-----+-----+-----+-----+-----+-----+-----+-----+-----+
+  |  _  |  ^  |  &  |  !  |  ?  |  ¥  |     |     |LEFT |DOWN |RGHT | END |     |     |
+  +-----+-----+-----+-----+-----+-----+-----+-----+-----+-----+-----+-----+-----+-----+
+  |     |     |     |  `  |  '  |  "  |     |     |     |     |     |     |     |     |
+  +-----+-----+-----+-----+-----+-----+-----+-----+-----+-----+-----+-----+-----+-----+
+  | __  |     |     |     | __  | __  | __  | __  | __  | __  |     |     |     | __  |
+  +-----+-----+-----+-----+-----+-----+-----+-----+-----+-----+-----+-----+-----+-----+
+*/
+  [_RAISE] = LAYOUT(
+    JP_PIPE,JP_TILD,JP_AT  ,JP_HASH,JP_DLR ,JP_PERC,                XXXXXXX,KC_UP  ,XXXXXXX,KC_HOME,XXXXXXX,XXXXXXX, \
+    JP_UNDS,JP_CIRC,JP_AMPR,JP_EXLM,JP_QUES,JP_YEN ,XXXXXXX,XXXXXXX,KC_LEFT,KC_DOWN,KC_RGHT,KC_END ,XXXXXXX,XXXXXXX, \
+                            JP_GRV ,JP_QUOT,JP_DQT ,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX     , \
+    _______,                        _______,_______,_______,_______,_______,_______,                        _______
+  ),
+
+/* _ADJUST
+  +-------+-------+-------+-------+-------+-------+-------+-------+-------+-------+-------+-------+-------+-------+
+  |ADV_ID0|ADV_ID1|ADV_ID2|       |       |BATT_LV|       |       |ENT_DFU|       |       |DEL_ID0|DEL_ID1|DEL_ID2|
+  +-------+-------+-------+-------+-------+-------+-------+-------+-------+-------+-------+-------+-------+-------+
+  |ADV_ID3|ADV_ID4|       |       |       | RESET |       |       |ENT_SLP|       |       |       |DEL_ID3|DEL_ID4|
+  +-------+-------+-------+-------+-------+-------+-------+-------+-------+-------+-------+-------+-------+-------+
+  |       |       |       |       |       |       |       |       |       |       |       |       |       |       |
+  +-------+-------+-------+-------+-------+-------+-------+-------+-------+-------+-------+-------+-------+-------+
+  |  __   |       |       |       |  __   |  __   |  __   |  __   |  __   |  __   |       |       |       |  __   |
+  +-------+-------+-------+-------+-------+-------+-------+-------+-------+-------+-------+-------+-------+-------+
+*/
+  [_ADJUST] = LAYOUT(
+    ADV_ID0,ADV_ID1,ADV_ID2,XXXXXXX,XXXXXXX,BATT_LV,                ENT_DFU,XXXXXXX,XXXXXXX,DEL_ID0,DEL_ID1,DEL_ID2, \
+    ADV_ID3,ADV_ID4,XXXXXXX,XXXXXXX,XXXXXXX,RESET  ,XXXXXXX,XXXXXXX,ENT_SLP,XXXXXXX,XXXXXXX,XXXXXXX,DEL_ID3,DEL_ID4, \
+                            XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX     , \
+    _______,                        _______,_______,_______,_______,_______,_______,                        _______
   ),
 
 };
