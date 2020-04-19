d27f0df06618bb65f1353b1bb1ac91a21a906d71
eswai <sakamoto.yuki@gmail.com>
2020-04-19 19:34:55 +0900

    LSFT_T

--- keyboards/eswai/gullwing/keymaps/default/keymap.c
+++ keyboards/eswai/gullwing/keymaps/default/keymap.c
@@ -77,7 +77,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_X   ,KC_Q   ,KC_W   ,KC_E   ,KC_R   ,KC_T   ,                KC_Y   ,KC_U   ,KC_I   ,KC_O   ,KC_P   ,JP_DOT , \
     KC_Z   ,KC_A   ,KC_S   ,KC_D   ,KC_F   ,KC_G   ,JP_COLN,JP_UNDS,KC_H   ,KC_J   ,KC_K   ,KC_L   ,JP_SCLN,JP_SLSH, \
                             KC_C   ,KC_V   ,KC_B   ,JP_LBRC,JP_RBRC,KC_N   ,KC_M   ,JP_COMM     , \
-    KC_LCMD,                        LOWER  ,KC_SPC ,KC_TAB ,KC_LALT,KC_ENT ,RAISE  ,                        KC_LCTL
+    KC_LCMD,                LOWER  ,LSFT_T(KC_SPC) ,KC_TAB ,KC_LALT,LSFT_T(KC_ENT) ,RAISE  ,                        KC_LCTL
   ),
 
 /* _LOWER
